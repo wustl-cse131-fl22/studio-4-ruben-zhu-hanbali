2d0e746869ff8f53b6e650b9bcdebd32ef092ec3
12034 <12034@DESKTOP-6QF6R4S.dhcp.wustl.edu>
2022-10-12 15:32:01 -0500

    done

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,7 +7,16 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
-		
-		
+		 StdDraw.setXscale(0, 22);
+		 StdDraw.setYscale(0, 16);
+		 
+		 StdDraw.setPenColor(100, 42, 201);
+		   StdDraw.filledRectangle(3, 3, 3, 3);
+		   StdDraw.filledRectangle(3, 13, 3, 3);
+		   StdDraw.filledRectangle(16, 3, 6, 3);
+		   StdDraw.filledRectangle(16, 13, 6, 3);
+		   StdDraw.setPenColor(75, 142, 127);
+		   StdDraw.filledRectangle(11, 8, 11, 1);
+		   StdDraw.filledRectangle(8, 8, 1, 8);
 	}
-}
+	}
